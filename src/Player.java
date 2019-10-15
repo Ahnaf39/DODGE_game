416216073a6adf33ffa48a54bf84f912f05e8f99
ShreyansK2000 <gshreyansk4@gmail.com>
2019-10-14 23:41:55 -0700

    first commit, moving square on screen

---
+++ src/Player.java
@@ -0,0 +1,18 @@
+import java.awt.*;
+
+public class Player extends GameObject{
+    public Player(int x, int y, ID id) {
+        super(x, y, id);
+        velX=1;
+    }
+
+    public void tick() {
+        x+=velX;
+        y+=velY;
+    }
+
+    public void render(Graphics g) {
+        g.setColor(Color.black);
+        g.fillRoundRect(x,y,32,32,10,10);
+    }
+}
