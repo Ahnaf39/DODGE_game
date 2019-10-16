74f2a6295957aa05d3a40b1331952d8e85a681ba
Ahnaf Ahsan <ahnaf452000@gmail.com>
2019-10-15 21:21:11 -0700

    Added Key input

---
+++ src/KeyInput.java
@@ -0,0 +1,77 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+    private Handler handler;
+    private boolean uP = false;
+    private boolean dP = false;
+    private boolean lP = false;
+    private boolean rP = false;
+    public KeyInput(Handler handler){
+        this.handler=handler;
+    }
+    public void keyPressed(KeyEvent e){
+        int key = e.getKeyCode();
+        //System.out.println(key);
+        for (int i=0; i< handler.object.size(); i++){
+            GameObject tempObject = handler.object.get(i);
+            if (tempObject.getId()==ID.Player){
+                //key events for player 1
+                if (key==KeyEvent.VK_UP){
+                    uP = true;
+                    tempObject.setVelY(-5);
+                }
+                if (key==KeyEvent.VK_DOWN){
+                    dP = true;
+                    tempObject.setVelY(5);
+                }
+                if (key==KeyEvent.VK_RIGHT){
+                    rP = true;
+                    tempObject.setVelX(5);
+                }
+                if (key==KeyEvent.VK_LEFT){
+                    lP = true;
+                    tempObject.setVelX(-5);
+                }
+            }
+        }
+    }
+    public void keyReleased(KeyEvent e){
+        int key = e.getKeyCode();
+
+        for (int i=0; i< handler.object.size(); i++){
+            GameObject tempObject = handler.object.get(i);
+            if (tempObject.getId()==ID.Player){
+                //key events for player 1
+                if (key==KeyEvent.VK_UP){
+                    uP=false;
+                    if(dP){
+                        tempObject.setVelY(5);
+                    }
+                    else{tempObject.setVelY(0);}
+                }
+                if (key==KeyEvent.VK_DOWN){
+                    dP=false;
+                    if(uP){
+                        tempObject.setVelY(-5);
+                    }
+                    else{tempObject.setVelY(0);}
+                }
+                if (key==KeyEvent.VK_RIGHT){
+                    rP=false;
+                    if(lP){
+                        tempObject.setVelX(-5);
+                    }
+                    else{tempObject.setVelX(0);}
+                }
+                if (key==KeyEvent.VK_LEFT){
+                    lP=false;
+                    if(rP){
+                        tempObject.setVelX(5);
+                    }
+                    else{tempObject.setVelX(0);}
+                }
+            }
+        }
+    }
+}
